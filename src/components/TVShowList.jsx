c131cb1382a56ed1f073c8cd5807457254184f4f
root <root@DESKTOP-ATOIEKT.localdomain>
2024-12-10 12:06:48 +0000

    added error handling

--- src/components/TVShowList.jsx
+++ src/components/TVShowList.jsx
@@ -7,9 +7,11 @@ import Format from "./Format";
 const TVShowList = ({ searchTerm }) => {
   const [shows, setShows] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
+    setIsError(false);
 
     fetch(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
       .then((response) => {
@@ -18,11 +20,18 @@ const TVShowList = ({ searchTerm }) => {
       .then((returnedShows) => {
         setShows(returnedShows);
         setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false)
+        setIsError(true)
       });
   }, [searchTerm]);
 
   if (isLoading) {
     return <Lottie animationData={loadingAnimation} />;
+  }
+  if (isError){
+    return <p>Error Returning Data</p>
   }
 
   return (
